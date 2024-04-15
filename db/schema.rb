7c87c770b682c6dcde81b7f9735297a361c9c7f5
marine gazier <hazier.mg@gmail.com>
2024-04-15 20:18:35 +0200

    date wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_110933) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_15_181357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_110933) do
     t.string "nom"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "day"
   end
 
   create_table "tasks", force: :cascade do |t|
